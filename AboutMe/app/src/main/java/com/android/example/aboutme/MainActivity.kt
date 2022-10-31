0e3a0801b2629b641cfd3ce5627db1b457f64f57
joeun-01 <jjy020121@gmail.com>
2022-11-01 04:06:49 +0900

    step 2.2

--- AboutMe/app/src/main/java/com/android/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/android/example/aboutme/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.android.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.android.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
+        binding.nicknameText.setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding.nicknameText.text = binding.nicknameEdit.text
+
+        binding.nicknameEdit.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname (view: View) {
+        binding.nicknameEdit.visibility = View.VISIBLE
+        binding.doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        binding.nicknameEdit.requestFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.nicknameEdit, 0)
     }
 }
